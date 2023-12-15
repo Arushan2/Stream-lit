fd1904481cef6e6754859b7b8c99c29eaaa43ba8
arushan2 <rockarush2@gmail.com>
2023-12-15 15:27:31 +0530

    make as can be download

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
 import streamlit as st
-x = st.number_input("Insert total marks as your sub scheme", value=None)
-y = st.number_input("Insert total marks as your main scheme", value=None)
+import pandas as pd
+from io import BytesIO
+
+# Inputs for the total marks in sub and main schemes, and the student's name and marks
+x = st.number_input("Insert total marks as your sub scheme", min_value=0, step=1, format='%d')
+y = st.number_input("Insert total marks as your main scheme", min_value=0, step=1, format='%d')
 name = st.text_input("Enter your Name")
-stmarks = st.number_input("Insert student's marks", value=None)
+stmarks = st.number_input("Insert student's marks", min_value=0, step=1, format='%d')
+
+# Function to calculate marks based on the schemes
 def calculate(sub_scheme, main_scheme, student_marks):
-    if sub_scheme != 0: 
+    if sub_scheme != 0:  # Prevent division by zero
         mainmarks = main_scheme / sub_scheme * student_marks
         return mainmarks
     else:
         return 0
-# all_inputs_provided = x > 0 and y > 0 and name and stmarks >= 0
-if st.button('Calculate Marks'):
-    calculated_marks = calculate(x, y, stmarks)
-    st.write(f"{name}'s calculated marks: {calculated_marks}")
+
+# Check if all inputs are provided
+all_inputs_provided = x > 0 and y > 0 and name and stmarks >= 0
+
+# Function to write data to a file and return a file-like object
+def to_excel(name, marks):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        pd.DataFrame({'Name': [name], 'Marks': [marks]}).to_excel(writer, index=False, sheet_name='Marks')
+    output.seek(0)
+    return output
+
+# Button to trigger the calculation and file download
+if all_inputs_provided:
+    if st.button('Calculate Marks'):
+        calculated_marks = calculate(x, y, stmarks)
+        st.write(f"{name}'s calculated marks: {calculated_marks}")
+
+        # Create an excel file and download button
+        file = to_excel(name, calculated_marks)
+        st.download_button(
+            label="Download Marks File",
+            data=file,
+            file_name="student_marks.xlsx",
+            mime="application/vnd.ms-excel"
+        )
 else:
-    st.write("Enter the details and press 'Calculate Marks' to see the result.")
+    st.write("Please enter all required fields to enable the calculation.")
