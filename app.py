ed1d460fde917079ae4546f047a74d12ca15478d
arushan2 <rockarush2@gmail.com>
2023-12-15 11:12:15 +0530

    rwfew

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 x = st.number_input("Insert total marks as your sub scheme", value=None)
 y = st.number_input("Insert total marks as your main scheme", value=None)
-name=st.text_input("Enter your Name", value=None)
+name=st.text_input("Enter your Name")
 stmarks=st.number_input("Insert student's marks", value=None)
-
+mainmarks=
 
 
 
