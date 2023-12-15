7351855cfe7ea9c806ba08e40aab9e0e75774c16
arushan2 <rockarush2@gmail.com>
2023-12-15 12:53:33 +0530

    e4fc4fce4

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ x = st.number_input("Insert total marks as your sub scheme", value=None)
 y = st.number_input("Insert total marks as your main scheme", value=None)
 name=st.text_input("Enter your Name")
 stmarks=st.number_input("Insert student's marks", value=None)
-st.button("Submit")
+butt=st.button("Submit")
+
 # def calculate(x,y,z):
     # mainmarks=y/x*stmarks
