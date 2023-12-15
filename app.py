dbc26f119fe1aa29188f8816c506f4083fc4f15c
arushan2 <rockarush2@gmail.com>
2023-12-15 13:02:11 +0530

    e5tgfdc

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
-x = st.number_input("Insert total marks as your sub scheme", value=None)
-y = st.number_input("Insert total marks as your main scheme", value=None)
-name=st.text_input("Enter your Name")
-stmarks=st.number_input("Insert student's marks", value=None)
-butt=st.button("Submit")
 
-# def calculate(x,y,z):
-    # mainmarks=y/x*stmarks
+# Inputs for the total marks in sub and main schemes, and the student's name and marks
+x = st.number_input("Insert total marks as your sub scheme", value=0)
+y = st.number_input("Insert total marks as your main scheme", value=0)
+name = st.text_input("Enter your Name")
+stmarks = st.number_input("Insert student's marks", value=0)
+
+# Function to calculate marks based on the schemes
+def calculate(sub_scheme, main_scheme, student_marks):
+    if sub_scheme != 0:  # Prevent division by zero
+        mainmarks = main_scheme / sub_scheme * student_marks
+        return mainmarks
+    else:
+        return 0
+
+# Button to trigger the calculation
+if st.button('Calculate Marks'):
+    calculated_marks = calculate(x, y, stmarks)
+    st.write(f"{name}'s calculated marks: {calculated_marks}")
+else:
+    st.write("Enter the details and press 'Calculate Marks' to see the result.")
