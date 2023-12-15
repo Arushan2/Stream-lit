dab756cde420c32856738f311738fbc543ffeca3
arushan2 <rockarush2@gmail.com>
2023-12-15 11:45:15 +0530

    detwg

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ y = st.number_input("Insert total marks as your main scheme", value=None)
 name=st.text_input("Enter your Name")
 stmarks=st.number_input("Insert student's marks", value=None)
 mainmarks=y/x*stmarks
-
+st.write(mainmarks)
 
 
 
