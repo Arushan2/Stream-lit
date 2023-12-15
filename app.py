e013d4821b4b8c5cc15815f103e24c9ee3b31112
arushan2 <rockarush2@gmail.com>
2023-12-15 11:07:13 +0530

    fwfwf

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 x = st.number_input("Insert total marks as your sub scheme", value=None)
 y = st.number_input("Insert total marks as your sub scheme", value=None)
-while(True):
-    name=st.text_input("Insert total marks as your sub scheme", value=None)
-    stmarks=st.number_input("Insert total marks as your sub scheme", value=None)
+name=st.text_input("Insert total marks as your sub scheme", value=None)
+stmarks=st.number_input("Insert total marks as your sub scheme", value=None)
 
 
 
