97dfcd85b5411d922524c5fced2b5bc961102d59
arushan2 <rockarush2@gmail.com>
2023-12-15 10:19:58 +0530

    project change panran

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 import streamlit as st
-st.write("Vanakkkam ulaham ")
-def calculate(a,b,c):
-    x=(-b+(b**2-4*a*c)**1/2)/2*a
-    return x
-userinputa=st.number_input('Insert a number a')
-userinputb=st.number_input('Insert a number b')
-userinputc=st.number_input('Insert a number c')
-st.write(calculate(userinputa,userinputb,userinputc))
+x=st.input("Please enter out marks with main scheme")
 
 
 
