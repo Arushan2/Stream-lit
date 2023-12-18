d84c74cc72a4a0fb53b0b1c84f5f6cc152a23834
arushan2 <rockarush2@gmail.com>
2023-12-18 21:09:05 +0530

    download version updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ x = st.number_input("Insert total marks as your sub scheme", value=None)
 y = st.number_input("Insert total marks as your main scheme", value=None)
 name = st.text_input("Enter your Name")
 stmarks = st.number_input("Insert student's marks", value=None)
-
+download_button=st.download_button(label="Download Marks")
 # Function to calculate the main marks based on the sub scheme, main scheme, and student's marks
 def calculate(sub_scheme, main_scheme, student_marks):
     if sub_scheme != 0: 
@@ -23,7 +23,7 @@ if st.button('Calculate Marks'):
     download_text = f"Name: {name}\nCalculated Marks: {calculated_marks}"
 
     # Creating a download button for the text
-    st.download_button(label="Download Marks", data=download_text, file_name=f"{name}_marks.txt", mime="text/plain")
+    download_button(label="Download Marks", data=download_text, file_name=f"{name}_marks.txt", mime="text/plain")
 
 else:
     st.write("Enter the details and press 'Calculate Marks' to see the result.")
