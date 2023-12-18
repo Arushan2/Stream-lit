6259faa4edadb48f16862cec124c5e2228aa41a2
arushan2 <rockarush2@gmail.com>
2023-12-18 21:16:10 +0530

    add download jason file

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
+import json
 
-# Input fields for total marks, student's name, and student's marks
+# Initialize a list to store student data
+students_data = []
+
 x = st.number_input("Insert total marks as your sub scheme", value=None)
 y = st.number_input("Insert total marks as your main scheme", value=None)
-name = st.text_input("Enter your Name")
-stmarks = st.number_input("Insert student's marks", value=None)
-download_button=st.download_button(label="Download Marks")
-# Function to calculate the main marks based on the sub scheme, main scheme, and student's marks
+
 def calculate(sub_scheme, main_scheme, student_marks):
     if sub_scheme != 0: 
         mainmarks = main_scheme / sub_scheme * student_marks
@@ -14,16 +14,28 @@ def calculate(sub_scheme, main_scheme, student_marks):
     else:
         return 0
 
-# Button to calculate and display the marks
-if st.button('Calculate Marks'):
+# Form for inputting student data
+with st.form("student_data_form"):
+    name = st.text_input("Enter your Name")
+    stmarks = st.number_input("Insert student's marks", value=None)
+    submitted = st.form_submit_button("Add Student")
+
+# Adding student data to the list
+if submitted:
     calculated_marks = calculate(x, y, stmarks)
+    students_data.append({"Name": name, "Marks": calculated_marks})
     st.write(f"{name}'s calculated marks: {calculated_marks}")
 
-    # Preparing the text to download
-    download_text = f"Name: {name}\nCalculated Marks: {calculated_marks}"
+# Button to download data as JSON
+if st.button('Download Data as JSON'):
+    # Convert student data to JSON
+    json_data = json.dumps(students_data, indent=4)
+    # Create a download button
+    st.download_button(label='Download JSON',
+                       data=json_data,
+                       file_name='students_data.json',
+                       mime='application/json')
 
-    # Creating a download button for the text
-    download_button(label="Download Marks", data=download_text, file_name=f"{name}_marks.txt", mime="text/plain")
-
-else:
-    st.write("Enter the details and press 'Calculate Marks' to see the result.")
+# Display message if the list is empty
+if not students_data:
+    st.write("Add students and calculate their marks to download the data.")
