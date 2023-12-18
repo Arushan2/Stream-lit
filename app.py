7801d14faaba9415c9c6b952720c61f510dc958f
arushan2 <rockarush2@gmail.com>
2023-12-18 16:16:17 +0530

    calculator

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import streamlit as st
+
+# Input fields for total marks, student's name, and student's marks
 x = st.number_input("Insert total marks as your sub scheme", value=None)
 y = st.number_input("Insert total marks as your main scheme", value=None)
 name = st.text_input("Enter your Name")
 stmarks = st.number_input("Insert student's marks", value=None)
+
+# Function to calculate the main marks based on the sub scheme, main scheme, and student's marks
 def calculate(sub_scheme, main_scheme, student_marks):
     if sub_scheme != 0: 
         mainmarks = main_scheme / sub_scheme * student_marks
         return mainmarks
     else:
         return 0
-# all_inputs_provided = x > 0 and y > 0 and name and stmarks >= 0
+
+# Button to calculate and display the marks
 if st.button('Calculate Marks'):
     calculated_marks = calculate(x, y, stmarks)
     st.write(f"{name}'s calculated marks: {calculated_marks}")
+
+    # Preparing the text to download
+    download_text = f"Name: {name}\nCalculated Marks: {calculated_marks}"
+
+    # Creating a download button for the text
+    st.download_button(label="Download Marks", data=download_text, file_name=f"{name}_marks.txt", mime="text/plain")
+
 else:
     st.write("Enter the details and press 'Calculate Marks' to see the result.")
