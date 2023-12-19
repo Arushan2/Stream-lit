a38abe0504f47b19d0204cb2d1098336ec868d6e
arushan2 <rockarush2@gmail.com>
2023-12-19 10:40:25 +0530

    add download button

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
-import json
+import pandas as pd
 
-# Function to calculate marks
+x = st.number_input("Insert total marks as your sub scheme", value=None)
+y = st.number_input("Insert total marks as your main scheme", value=None)
+name = st.text_input("Enter your Name")
+stmarks = st.number_input("Insert student's marks", value=None)
+
+# DataFrame to store the results
+results_df = pd.DataFrame(columns=['Name', 'Calculated Marks'])
+
 def calculate(sub_scheme, main_scheme, student_marks):
     if sub_scheme != 0: 
         mainmarks = main_scheme / sub_scheme * student_marks
@@ -9,34 +16,25 @@ def calculate(sub_scheme, main_scheme, student_marks):
     else:
         return 0
 
-# Store student data
-student_data = []
-
-# Input fields
-x = st.number_input("Insert total marks as your sub scheme", value=None)
-y = st.number_input("Insert total marks as your main scheme", value=None)
-name = st.text_input("Enter your Name")
-stmarks = st.number_input("Insert student's marks", value=None)
-
-# Button to calculate marks
 if st.button('Calculate Marks'):
     calculated_marks = calculate(x, y, stmarks)
     st.write(f"{name}'s calculated marks: {calculated_marks}")
-    # Add student data to the list
-    student_data.append({"Name": name, "Marks": calculated_marks})
+    # Append the result to the DataFrame
+    results_df.loc[len(results_df)] = [name, calculated_marks]
+    # Display the DataFrame
+    st.write(results_df)
 else:
     st.write("Enter the details and press 'Calculate Marks' to see the result.")
 
-# Function to download data as JSON
-def download_json(data):
-    json_data = json.dumps(data)
-    st.download_button(
-        label="Download Data as JSON",
-        data=json_data,
-        file_name='student_data.json',
-        mime='application/json'
-    )
+# Function to convert DataFrame to CSV for download
+def convert_df_to_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
 
-# Show download button
-if student_data:
-    download_json(student_data)
+# Download button for the table as CSV
+csv = convert_df_to_csv(results_df)
+st.download_button(label="Download Results as CSV", data=csv, file_name='calculated_marks.csv', mime='text/csv')
+
+# Print button for the table
+print_button = st.button("Print Table")
+if print_button:
+    st.write(results_df)
