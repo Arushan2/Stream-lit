71eca3c9ec419b39ded15beff5c059e626ac8e1d
arushan2 <rockarush2@gmail.com>
2023-12-19 11:02:34 +0530

    add download button

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+# Initialize the DataFrame outside of the main loop
+if 'results_df' not in st.session_state:
+    st.session_state.results_df = pd.DataFrame(columns=['Name', 'Calculated Marks'])
+
 x = st.number_input("Insert total marks as your sub scheme", value=0)
 y = st.number_input("Insert total marks as your main scheme", value=0)
 name = st.text_input("Enter your Name")
 stmarks = st.number_input("Insert student's marks", value=0)
-
-# DataFrame to store the results
-results_df = pd.DataFrame(columns=['Name', 'Calculated Marks'])
 
 def calculate(sub_scheme, main_scheme, student_marks):
     if sub_scheme != 0: 
@@ -20,9 +21,9 @@ if st.button('Calculate Marks'):
     calculated_marks = calculate(x, y, stmarks)
     st.write(f"{name}'s calculated marks: {calculated_marks}")
     # Append the result to the DataFrame
-    results_df.loc[len(results_df)] = [name, calculated_marks]
+    st.session_state.results_df.loc[len(st.session_state.results_df)] = [name, calculated_marks]
     # Display the DataFrame
-    st.write(results_df)
+    st.write(st.session_state.results_df)
 else:
     st.write("Enter the details and press 'Calculate Marks' to see the result.")
 
@@ -30,19 +31,21 @@ else:
 def convert_df_to_json(df):
     return df.to_json(orient='records').encode('utf-8')
 
-# Download button for the table as JSON
-json_data = convert_df_to_json(results_df)
-st.download_button(label="Download Results as JSON", data=json_data, file_name='calculated_marks.json', mime='application/json')
-
 # Function to convert DataFrame to CSV for download
 def convert_df_to_csv(df):
     return df.to_csv(index=False).encode('utf-8')
 
-# Download button for the table as CSV
-csv = convert_df_to_csv(results_df)
-st.download_button(label="Download Results as CSV", data=csv, file_name='calculated_marks.csv', mime='text/csv')
+# Download buttons
+if len(st.session_state.results_df) > 0:
+    # Download button for the table as JSON
+    json_data = convert_df_to_json(st.session_state.results_df)
+    st.download_button(label="Download Results as JSON", data=json_data, file_name='calculated_marks.json', mime='application/json')
+
+    # Download button for the table as CSV
+    csv = convert_df_to_csv(st.session_state.results_df)
+    st.download_button(label="Download Results as CSV", data=csv, file_name='calculated_marks.csv', mime='text/csv')
 
 # Print button for the table
 print_button = st.button("Print Table")
 if print_button:
-    st.write(results_df)
+    st.write(st.session_state.results_df)
